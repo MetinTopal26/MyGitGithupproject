13ca0ecf06c2510f58fdc3c429f288f1b2f9db51
metin <metintopal777@gmail.com>
2023-04-26 21:00:18 +0200

    V2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -3,5 +3,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir.");
         //version1
+        System.out.println("Java is easy");
+        //versiyon2
     }
 }
